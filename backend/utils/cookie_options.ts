7c81833f9f04916fb4f1e637daec2be51938612b
sd <ab@cd.com>
2026-07-24 18:04:37 +0545

    cookieoption update

--- backend/utils/cookie_options.ts
+++ backend/utils/cookie_options.ts
@@ -17,6 +17,22 @@
 //   maxAge: validMaxAge,
 // };
 
+// import { ACCESS_COOKIE_MAXAGE, NODE_ENV } from '../config/utils.js';
+
+// const defaultMaxAge = 3600000;
+// const maxAge =
+//   typeof ACCESS_COOKIE_MAXAGE === 'string' ? parseInt(ACCESS_COOKIE_MAXAGE, 10) : defaultMaxAge;
+// const validMaxAge = isNaN(maxAge) ? defaultMaxAge : maxAge;
+
+// const isHttps = process.env.USE_HTTPS === 'true';
+
+// export const cookieOptions = {
+//   httpOnly: true,
+//   sameSite: 'lax',
+//   secure: isHttps,
+//   maxAge: validMaxAge,
+// };
+
 import { ACCESS_COOKIE_MAXAGE, NODE_ENV } from '../config/utils.js';
 
 const defaultMaxAge = 3600000;
@@ -26,7 +42,14 @@ const validMaxAge = isNaN(maxAge) ? defaultMaxAge : maxAge;
 
 const isHttps = process.env.USE_HTTPS === 'true';
 
-export const cookieOptions = {
+interface CookieObject {
+  httpOnly: boolean;
+  sameSite: 'lax' | 'strict' | 'none';
+  secure: boolean;
+  maxAge: number;
+}
+
+export const cookieOptions: CookieObject = {
   httpOnly: true,
   sameSite: 'lax',
   secure: isHttps,
